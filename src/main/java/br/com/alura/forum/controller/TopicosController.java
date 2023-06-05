cd26e6ead21cce011df51b1515a312e280ebb4d3
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-05 19:28:43 -0300

    import  TopicosRepository list all Topicos and add filter cursoNome

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,20 +3,30 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-		List<Topico> topicos = Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> lista(String cursoNome) {
+		List<Topico> topicos;
+		
+		if (cursoNome == null) {
+			topicos = topicoRepository.findAll();			
+		} else {
+			topicos = topicoRepository.findByCurso_Nome(cursoNome);
+		}
 		
 		return TopicoDto.converter(topicos); 
 		
