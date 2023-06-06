c9ed86960715402c9694d0c118a07bf85736ec37
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-06 11:56:44 -0300

    create endpoint PUT /topicos/{id}

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import br.com.alura.forum.controller.dto.TopicoDetailsDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.controller.form.TopicoUpdateForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
@@ -35,7 +38,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> lista(String cursoNome) {
+	public List<TopicoDto> listTopics(String cursoNome) {
 		List<Topico> topicos;
 		
 		if (cursoNome == null) {
@@ -49,7 +52,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm  form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDto> createTopic(@RequestBody @Valid TopicoForm  form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
@@ -59,10 +62,21 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
-	public TopicoDetailsDto getTopico(@PathVariable Long id) {
+	public TopicoDetailsDto showTopic(@PathVariable Long id) {
 		Topico topico = topicoRepository.getOne(id);
 		
 		return new TopicoDetailsDto(topico);
 	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> updateTopic(@PathVariable Long id, @RequestBody @Valid TopicoUpdateForm  form) {
+		
+		Topico topico = form.updateTopic(id, topicoRepository);
+		
+		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	
 }
  
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoUpdateForm.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class TopicoUpdateForm {
+
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public Topico updateTopic(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getOne(id);
+		topico.setMensagem(this.getMensagem());
+		topico.setTitulo(this.getTitulo());
+		
+		return topico;
+	}
+	
+	
+	
+	
+	
+	
+}
