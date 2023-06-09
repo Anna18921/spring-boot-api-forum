280c0021c3bcf6b2d04ced802277969ed4276f67
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-09 14:24:59 -0300

    config login spring security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$mmOFfSOEJdj8C.QQCZ/pTuO0VVm1Bh1004ff8pcIHGr0ixaG2GhFC');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
