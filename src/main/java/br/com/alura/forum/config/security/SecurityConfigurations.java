3256f4a3de6a73a6d1a931e3ff7eb6bbf8b2181e
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-12 20:37:58 -0300

    configure swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -58,7 +58,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Congiurações recursos estaticos(ex: requisicao de css, javascript, imagens etc ...)
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring().antMatchers("/**.html**", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
 }
